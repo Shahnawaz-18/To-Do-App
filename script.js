f00d8d549d9759e262a9df0fbef90e36e6abaef4
Shahnawaz Akhtar <sakhtar18012004@gmail.com>
2025-07-11 10:13:21 +0530

    commit

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const taskInput= document.getElementById("taskInput");
+const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
-window.onload = () =>{
-    loadTasks();
+window.onload = () => {
+  loadTasks();
 };
 
-function addtask() {
-    const taskText= taskInput.ariaValueMax.trim();
-    if (taskText== "") return;
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
 
-    const li=document.createElement("li")
-    li.innerHTML =`
+  const li = document.createElement("li");
+  li.innerHTML = `
     <span onclick="toggleComplete(this)">${taskText}</span>
     <button onclick="deleteTask(this)"><i class="fa-solid fa-trash"></i></button>
-    `;
-    taskList.appendChild(li);
-    taskInput.value = "";
+  `;
+  taskList.appendChild(li);
+  taskInput.value = "";
 
-    saveTask();
+  saveTasks();
 }
 
 function toggleComplete(span) {
-    span.parentElement.classList.toggle("Completed");
-    saveTask();
+  span.parentElement.classList.toggle("completed");
+  saveTasks();
 }
 
 function deleteTask(button) {
-    button.parentElement.remove();
-    saveTask();
+  button.parentElement.remove();
+  saveTasks();
 }
 
-function saveTask() {
-    localStorage.setItem("tasks", taskList.innerHTML);
+function saveTasks() {
+  localStorage.setItem("tasks", taskList.innerHTML);
 }
 
 function loadTasks() {
-    const data=localStorage.getItem("tasks")
-    if(data) taskList.innerHTML = data;
+  const data = localStorage.getItem("tasks");
+  if (data) taskList.innerHTML = data;
 }
